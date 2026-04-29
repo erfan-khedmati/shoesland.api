fc006ddfa02b57ee598cba423e302c91348e8840
erfan-khedmati <erfankhedmati15@gmail.com>
2026-04-29 20:12:51 +0330

    add cors

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require('cors');
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', "Authorization"]
+}))
 app.use(express.json());
 app.use(cookieParser());
 
